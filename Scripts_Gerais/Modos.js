83b8aa428e05a2942abe9e2252b9cde68ecc9e86
João Paulo Filipini <filipinijoaopaulo@gmail.com>
2025-09-06 10:21:28 -0300

    Add files via upload

--- Scripts_Gerais/Modos.js
+++ Scripts_Gerais/Modos.js
@@ -2,10 +2,13 @@
 
 function mudarModo() {
     let estilo = document.getElementById("EstiloPagina");
+    let botao = document.getElementById("btnMudarModo");
 
     if(estilo.href.includes("ModoEscuro.css") ) {
         estilo.href = "../CSS/ModoClaro.css";
+        botao.innerText = "Mudar para modo escuro";
     } else if(estilo.href.includes("ModoClaro.css") ) {
         estilo.href = "../CSS/ModoEscuro.css";
+        botao.innerText = "Mudar para modo claro";
     }
 }
